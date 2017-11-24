3c2a06ad46df5f3fd12f0be9afd437add97aaaa9
Henrik Fricke <henrik.fricke@jimdo.com>
2017-11-24 17:47:00 +0100

    Reset add todo form

--- app/addTodo/addTodo.js
+++ app/addTodo/addTodo.js
@@ -14,13 +14,22 @@ class HfAddTodo extends HTMLElement {
     }
     
     connectedCallback() {
+        this.addListener()
+    }
+
+    addListener() {
         this.shadow.querySelector('input').addEventListener('change', this.inputChanged)
         this.shadow.querySelector('button').addEventListener('click', this.buttonClicked)
     }
 
+    removeListener() {
+        this.shadow.querySelector('input').removeEventListener('change', this.inputChanged)
+        this.shadow.querySelector('button').removeEventListener('click', this.buttonClicked)
+    }
+
     resetValue() {
         this.state.value = ''
-        this.render()
+        this.rerender()
     }
 
     inputChanged(event) {
@@ -29,16 +38,29 @@ class HfAddTodo extends HTMLElement {
 
     buttonClicked(event) {
         window.state.addTodoItem(this.state.value)
+        this.resetValue()
+    }
+
+    rerender() {
+        this.removeListener()
+        this.renderForm()
+        this.addListener()
+    }
+
+    renderForm() {
+        this.shadow.querySelector('.hf-add-todo').innerHTML = `
+            <input class="hf-add-todo__input" type="text" value="${this.state.value}" />
+            <button class="hf-add-todo__button">Add</button>
+        `
     }
 
     render() {
         this.shadow.innerHTML = `
-            <div class="hd-add-todo">
-                <input class="hd-add-todo__input" type="text" value="${this.state.value}" />
-                <button class="hd-add-todo__button">Add</button>
-            </div>
+            <div class="hf-add-todo"></div>
             <link rel="stylesheet" href="addTodo/addTodo.css">
         `;
+
+        this.renderForm()
     }
 }
 
